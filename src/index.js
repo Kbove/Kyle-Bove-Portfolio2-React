1bd82f4368f99e7715d65b5c1a185f70a7162ee3
Kbove <kbove94@gmail.com>
2021-12-12 13:12:57 -0800

    fixing memory issue

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import v8 from 'v8'
+
+// const totalHeapSize = v8.getHeatStatistics().total_available_size
+// let totalHeapSizeInGB = (totalHeapSize / 1024 / 1024 / 1024).toFixed(2)
+
+// console.log(`total heap size (bytes) ${totalHeapSize}, (GB${totalHeapSizeInGB})`)
 
 ReactDOM.render(<App />, document.getElementById('root'));
